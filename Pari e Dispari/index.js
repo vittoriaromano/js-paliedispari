cf26611758d888e90c7152d624938c36e5e34c95
vittoriaromano <vittoria5005@gmail.com>
2023-03-01 20:19:24 +0400

    pali-fix

--- Pari e Dispari/index.js
+++ Pari e Dispari/index.js
@@ -1,15 +1,28 @@
 'use strict'
 
 //funzione per sommare i numeri facilmente dopo (FUNZIONE 1)
-function sommaNumeri(num1, num2 = 0) {
-    return num1 + num2;
-}
+// function sommaNumeri(num1, num2 = 0) {
+//     return num1 + num2;
+// }
 
 //funzione per i numeri pari o dispari (FUNZIONE 2)
-function evenOdd(){
-    if(sommaNumeri % 2 === 0 ){
-        console.log('even')
-    }else{
-        console.log('odd')
-    }
+
+
+//prompt scegliere pari o dispari poi
+const numer = prompt('scegli un numero');
+console.log(numer);
+
+const evenOdd = prompt('pari o dispari');
+console.log(evenOdd);
+
+if(evenOdd != pari || evenOdd != pari){
+    alert('pari o dispari');
 }
+
+// function evenOdd(){
+//     if(sommaNumeri % 2 === 0 ){
+//         console.log('even')
+//     }else{
+//         console.log('odd')
+//     }
+// }
--- pali/index.js
+++ pali/index.js
@@ -3,23 +3,30 @@
 //prompt per avere una parola in una variabile
 let parola = prompt('inserisci parola');
 
+let prima = '';
+let seconda = '';
 
 //con un ciclo stampiano la parola come se fosse un array
-function normale (){
+ 
 for(let i = 0; i < parola.length; i++){
-}
+    prima = parola[i];
+    console.log(prima);
+
+    
 }
 
 //poi con un altro ciclo lo stampiamo al contrario
-function rovescio (){
-for(i < parola.length -1;i >= 0; i--){
-}
-}
 
+for(let i = parola.length -1; i >= 0; i--){
+    seconda = parola[i];
+    console.log(seconda);
+
+}
+ 
 //se corrispondono sono Pali
-if(normale == rovescio){
+if(prima === seconda ){
     console.log('pali')
-     //se non corrispondono non sono Pali
-}else if(normale !== rovescio){
+    //se non corrispondono non sono Pali
+}else{
     console.log('no pali')
 }
